126301c73462b057b70b3ba5148209757d878e66
joshisuryansh <joshisuryansh41@gmail.com>
2024-11-08 13:31:02 +0530

    modified the readme and a python file

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# demo_test_repo
+# First demo project.
+
 This is a demo repository
+- first we created .gitignore, LICENSE, README.md from github
+- second for commiting and pushing changes :
+   - git add .
+   - git status 
+   - git commit -m "your commit message goes here"
+   - git push origin main
--- new_x.py
+++ new_x.py
@@ -1,5 +1,14 @@
 # updating the file
 
-def calc(num1 : float , num2 : float ,operator : str):
+from typing import Union
+
+def calc(num1: Union[float, int], num2: Union[float, int], operator: str) -> Union[float, int]:
     if operator == "+":
         return num1 + num2
+    if operator == "-":
+        return num1 - num2
+    if operator == "*":
+        return num1 * num2
+    if operator == "/":
+        return num1 / num2
+
